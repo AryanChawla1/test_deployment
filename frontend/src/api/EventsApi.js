e1e5657d6018317e73473df3a525237e4096b5c8
Josh Cowan <35657686+joshuajz@users.noreply.github.com>
2023-11-28 15:50:41 -0500

    Environment Variables

--- frontend/src/api/EventsApi.js
+++ frontend/src/api/EventsApi.js
@@ -1,14 +1,14 @@
-const URL = 'http://localhost:3001'
+const URL = `${process.env.REACT_APP_BACKEND_URL}`;
 
 const eventPost = async (id, newValue) => {
   const requestOptions = {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ id: id, registered: newValue }),
-  }
+  };
 
-  const fetched = await fetch(`${URL}/events`, requestOptions)
-  const json = await fetched.json()
-  return json
-}
-export default eventPost
+  const fetched = await fetch(`${URL}/events`, requestOptions);
+  const json = await fetched.json();
+  return json;
+};
+export default eventPost;
